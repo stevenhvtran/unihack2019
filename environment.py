5b6ba63f729f8396acac312092da81df788e26fc
stevenhvtran <stransteven@outlook.com>
2019-03-16 19:02:06 +1100

    Updated environments and added machine learning model

--- environment.py
+++ environment.py
@@ -11,43 +11,49 @@ light_dict = {
     1: np.array([0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0]),
     2: np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0]),
     3: np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]),
-    4: np.array([0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
-    5: np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]),
-    6: np.array([1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]),
-    7: np.array([0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0])
+    4: np.array([1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]),
+    5: np.array([0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0])
 }
 
 
 class TrafficEnv:
     def __init__(self):
-        self.lanes = None
-        self.light_cfg = None
+        self.lanes = [Lane()] * 12
+        self.light_cfg = 0
         self.reset()
 
         # By default spawn a new car 30% of the time
         # ordered by Roads, N, E, S, W
-        self.car_density = [0.3, 0.3, 0.3, 0.3]
+        self.car_density = [0.4 for _ in range(4)]
 
     def reset(self):
-        self.lanes = [Lane()] * 12
+        self.lanes = [Lane() for _ in range(12)]
         self.light_cfg = 0
 
     def step(self, next_light_cfg):
-        self.release_cars(next_light_cfg)
+        num_cars_out = self.release_cars(next_light_cfg)
         new_cars = self.spawn_cars()
         self.light_cfg = next_light_cfg
-        return new_cars
+        return new_cars, num_cars_out
 
     def spawn_cars(self):
         new_cars = []
-        # Do randomised spawns for simulator
-        for lane_num, lane in enumerate(self.lanes):
-            road_num = lane_num // 3
-            if np.random.random() < self.car_density[road_num]:
-                turn_direction = self.get_direction(lane_num)
-                self.lanes[lane_num].add_car(Car(turn_direction))
-                new_cars.append((lane_num, turn_direction))
+        for road_num in range(4):
+            spawn_prob = self.car_density[road_num]
+            if np.random.random() < spawn_prob:
+                # Pick most empty lane out of the 3 lanes
+                empty_lane = self.get_most_empty_lane_num(road_num)
+                turn_direction = self.get_direction(empty_lane)
+                self.lanes[empty_lane].add_car(Car(turn_direction))
+                new_cars.append((empty_lane, turn_direction))
+
         return new_cars
+
+    def get_most_empty_lane_num(self, road_num):
+        lane_states = [lane.num_cars for lane in self.lanes]
+        min_local_lane = np.argmin(lane_states[road_num * 3:road_num * 3 + 3])
+        empty_lane_num = road_num * 3 + min_local_lane
+        return empty_lane_num
 
     def get_direction(self, lane_num):
         # Left turning lane
@@ -68,13 +74,18 @@ class TrafficEnv:
         return turn_direction
 
     def release_cars(self, next_cfg):
+        num_cars_out = 0
         old_config = light_dict[self.light_cfg]
         new_config = light_dict[next_cfg]
         open_lanes = np.multiply(old_config, new_config)
 
         for lane_num, release in enumerate(open_lanes):
-            if release:
+            if release and self.lanes[lane_num].queue:
                 self.lanes[lane_num].pop_car()
+                num_cars_out += 1
+
+        return num_cars_out
+
 
 
 class Car:
